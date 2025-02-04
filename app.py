2ead8bf4833ce58c94f9a659aaeb72c0ea9f6b28
amirmahdi-alavi <sam.alavi@hotmail.com>
2025-02-04 18:28:57 -0500

    add supabase connection

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
+from dotenv import load_dotenv
+import psycopg2
+
+
+load_dotenv()
+
+connection = psycopg2.connect(
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME
+    )
 
 app = Flask(__name__)
 
+cursor = connection.cursor()
+
 @app.route("/")
 def home():
-    return "Welcome to Flask on Heroku!"
+    cursor.execute("select * from schools;")
+    result = cursor.fetchone()
+    return "Welcome to Flask on Heroku {}!".format(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
