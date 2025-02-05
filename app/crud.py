a2f8af98e6756a318fe93a86f13b6a2a3d68f7c1
amirmahdi-alavi <sam.alavi@hotmail.com>
2025-02-05 13:52:06 -0500

    [bug] fixed connection error

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 from app.models import ParticipantInformation
 
 
 def get_information(db: Session):
-    return db.query(ParticipantInformation).all()
+    return db.execute(text('select * from participant_informations')).all()
