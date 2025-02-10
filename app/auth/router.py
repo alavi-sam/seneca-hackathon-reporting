0f22544acda956321b92995c2c5ebd1c6238b274
amirmahdi-alavi <sam.alavi@hotmail.com>
2025-02-10 01:28:51 -0500

    [feature] edited main.html

--- app/auth/router.py
+++ app/auth/router.py
@@ -6,16 +6,16 @@ from sqlalchemy.orm import Session
 from fastapi.responses import HTMLResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from app.auth.services import get_user, create_access_token
-from app.auth.dependencies import get_current_user
+
 
 
 auth_router = APIRouter()
-auth_templates = Jinja2Templates(directory='app/auth/static')
+static_templates = Jinja2Templates(directory='app/statics')
 
 
 @auth_router.get("/login", response_class=HTMLResponse)
 async def login_page(request: Request):
-    return auth_templates.TemplateResponse("login.html", {"request": request})
+    return static_templates.TemplateResponse("login.html", {"request": request})
 
 
 @auth_router.post('/login')
@@ -25,7 +25,7 @@ async def login_user(request: Request, form_data: OAuth2PasswordRequestForm = De
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
     access_token = create_access_token(data={'sub': user.username})
-    response = RedirectResponse(url='/main', status_code=status.HTTP_303_SEE_OTHER)
+    response = RedirectResponse(url='/', status_code=status.HTTP_303_SEE_OTHER)
     response.set_cookie(
         key = "access_token",
         value = f"Bearer {access_token}",
@@ -34,8 +34,4 @@ async def login_user(request: Request, form_data: OAuth2PasswordRequestForm = De
     )
     return response
 
-
-# @auth_router.get('/index', response_class=HTMLResponse)
-# async def index_page(request: Request, user: dict = Depends(get_current_user)):
-#     return auth_templates.TemplateResponse("index.html", {"request": request, "username": user["username"]})
     
--- app/main.py
+++ app/main.py
@@ -6,12 +6,16 @@ from jose import JWTError
 from sqlalchemy.orm import Session
 from app.auth.router import auth_router
 from dotenv import load_dotenv
+from fastapi.templating import Jinja2Templates
 
 load_dotenv()
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth")
+static_templates = Jinja2Templates(directory='app/statics')
+
+
 
 @app.get('/index')
 async def get_index(request: Request, user=Depends(check_for_login)):
@@ -27,18 +31,6 @@ async def get_main(request: Request, user=Depends(check_for_login)):
     if not user:
         return RedirectResponse("/auth/login")
     print(user)
-    return HTMLResponse(f"""
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    testttttttttt {user['username']}
-</body>
-</html>
-
-""")
+    return static_templates.TemplateResponse('main.html',
+                                             {"request": request, "user": {"username": user['username']}}
+)
