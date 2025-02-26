1fa3120af3c04040cf7418b9d191392ed98938b1
Sam Alavi <sam.alavi@hotmail.com>
2025-02-25 19:12:15 -0500

    school table full load

--- Airflow/dags/schools_full_load.py
+++ Airflow/dags/schools_full_load.py
@@ -2,9 +2,15 @@ from airflow import DAG
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.hooks.base import BaseHook
+import psycopg2
 import datetime as dt
 import pandas as pd
+import os
 import io
+
+
+BASE_PATH = os.path.join(os.getenv('AIRFLOW_HOME'), 'dags')
 
 
 default_args = {
@@ -28,9 +34,18 @@ start_dag = EmptyOperator(
     dag=dag
 )
 
+
+truncate_school_report = SQLExecuteQueryOperator(
+    conn_id='supabase_report',
+    sql='sqlScripts/truncateSchoolDest.sql',
+    task_id='truncate_report_school',
+    dag=dag
+)
+
+
 fetch_school_data = SQLExecuteQueryOperator(
     conn_id='supabase_prod',
-    sql='./sqlScripts/getSchool.sql',
+    sql='sqlScripts/getSchoolSource.sql',
     do_xcom_push=True,
     dag=dag,
     task_id='fetch_school_from_prod'
@@ -45,5 +60,52 @@ def _transform_school(**kwargs):
     column_names = ['school_id', 'school_name', 'province', 'city']
     df = pd.DataFrame(fetched_data, columns=column_names)
 
+    os.makedirs(os.path.join(os.getenv('AIRFLOW_HOME'), 'dags', 'tempFiles'), exist_ok=True)
+    df.to_csv(os.path.join(os.getenv('AIRFLOW_HOME'), 'dags', 'tempFiles', 'SourceSchool.csv'), index=False)
 
+
+def _insert_school_table(**kwargs):
+    report_conn = BaseHook.get_connection('supabase_report')
+    conn = psycopg2.connect(
+        host=report_conn.host,
+        port=report_conn.port,
+        database=report_conn.schema,
+        user=report_conn.login,
+        password=report_conn.password
+    )
+
+    cursor = conn.cursor()
+
+    with open(os.path.join(BASE_PATH, 'tempFiles', 'SourceSchool.csv'), 'r') as f:
+        cursor.copy_expert("""
+        COPY school FROM STDIN WITH CSV HEADER DELIMITER ',';
+                            """, file=f)
+        
+    conn.commit()
+    cursor.close()
+
+transform_school_data = PythonOperator(
+    python_callable=_transform_school,
+    dag=dag,
+    task_id='transform_school_data'
+)
+
+
+load_school = PythonOperator(
+    python_callable=_insert_school_table,
+    task_id='load_school_report',
+    dag=dag
+)
+
+
+
+
+end_task = EmptyOperator(
+    task_id='ending_task',
+    dag=dag
+)
+
+
+
+start_dag >> fetch_school_data >> transform_school_data >> load_school >> end_task
 
---
+++ Airflow/dags/sqlScripts/getSenecaProgramsSource.sql
@@ -0,0 +1,1 @@
+select * from seneca_programs;
